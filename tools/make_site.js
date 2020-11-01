69a607e36f27ca768aa10e8df0ceaef8fcaadd0f
LingDong- <lingdong0618@hotmail.com>
2020-10-31 21:48:42 -0400

    site gh button

--- tools/make_site.js
+++ tools/make_site.js
@@ -123,7 +123,27 @@ var html = `
   <div id="cr" style="position:absolute;left:50%;top:50px;width:50%;height:calc(100% - 50px)">
     
   </div>
+  <div id="ghub" style="opacity:50%;position:absolute;right:9px;top:8px;cursor:pointer;z-index:1000">
+<svg fill="white" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0" y="0" width="32" height="32" viewBox="0, 0, 32, 32">
+    <path d="M15.999,-0 C7.163,-0 0,7.345 0,16.405 C0,23.653 4.584,29.802 10.942,31.972 C11.743,32.122 12.034,31.615 12.034,31.18 C12.034,30.792 12.021,29.759 12.013,28.391 C7.562,29.382 6.623,26.191 6.623,26.191 C5.895,24.296 4.846,23.791 4.846,23.791 C3.394,22.774 4.956,22.794 4.956,22.794 C6.562,22.91 7.407,24.485 7.407,24.485 C8.834,26.992 11.152,26.268 12.064,25.848 C12.209,24.788 12.622,24.065 13.079,23.655 C9.527,23.242 5.791,21.834 5.791,15.547 C5.791,13.757 6.415,12.292 7.438,11.145 C7.273,10.73 6.724,9.063 7.595,6.804 C7.595,6.804 8.938,6.363 11.995,8.486 C13.271,8.121 14.64,7.94 16,7.934 C17.359,7.94 18.728,8.121 20.006,8.486 C23.061,6.363 24.401,6.804 24.401,6.804 C25.275,9.063 24.726,10.73 24.561,11.145 C25.586,12.292 26.206,13.757 26.206,15.547 C26.206,21.85 22.465,23.236 18.9,23.642 C19.475,24.149 19.986,25.15 19.986,26.681 C19.986,28.873 19.967,30.643 19.967,31.18 C19.967,31.619 20.255,32.13 21.067,31.97 C27.42,29.796 32,23.651 32,16.405 C32,7.345 24.836,-0 15.999,-0"/>
+</svg>
+  </div>
 </body>
+<script>
+var ghub = document.getElementById("ghub");
+ghub.onmouseenter=function(){ghub.style.opacity=0.8}
+ghub.onmouseleave=function(){ghub.style.opacity=0.5}
+ghub.onclick=function(){document.location.href="https://github.com/LingDong-/wax"}
+function ghubresize(){
+  if (window.innerWidth<1024){
+    ghub.style.top="58px";
+  }else{
+    ghub.style.top="8px";
+  }
+}
+ghubresize();
+window.addEventListener('resize', ghubresize);
+</script>
 <script>
   var examples = ${JSON.stringify(examples)}
 </script>
